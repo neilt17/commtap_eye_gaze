a9cb6472c9503822d013eeeca2ff3611714e13cd
Neil Thompson <neil.thompson@commtap.org>
2016-02-19 16:07:02 +0000

    Audio play for iPad (etc.)
    Adding in autoplay when adding controls to an element.

--- test-eg2016-files/static/resources/board.js
+++ test-eg2016-files/static/resources/board.js
@@ -88,7 +88,8 @@ var App = {
     if (endangle > Math.PI * 2) {
       var audio = $('audio', context).get(0);
       if (audio !== undefined) {
-        $('audio', context).attr('controls', 'true');
+        $('audio', context).attr('autoplay', 'autoplay');
+        $('audio', context).attr('controls', 'controls');
         audio.load();
         App.playWhenReady(audio);
         //$('audio', context).removeAttr('controls');
