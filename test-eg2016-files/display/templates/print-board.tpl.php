10245cb0d5e34aa5eda583c0393874c7c6f1321c
Neil Thompson <neil.thompson@commtap.org>
2016-03-16 09:40:42 +0000

    Creating e-tran print out pages: Added directories for testing; Initial set up.

---
+++ test-eg2016-files/display/templates/print-board.tpl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title>Communication Board</title>
+  <link href="static/resources/board.css" rel="stylesheet">
+  <!-- If using wih Drupal, say, ensure the page loads this version of jQuery
+  rather than the usual one -->
+
+</head>
+<body>
+<div id="board">
+<?php print $content ?>
+</div>
+</body>
+</html>
