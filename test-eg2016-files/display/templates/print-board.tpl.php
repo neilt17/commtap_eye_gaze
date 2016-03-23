d33e8d8361b1f9df1722e0d0916082ed5c5fb718
Neil Thompson <neil.thompson@commtap.org>
2016-03-23 09:40:50 +0000

    Started to put together printing of etran (in docx) - making a print layout using a table.

--- test-eg2016-files/display/templates/print-board.tpl.php
+++ test-eg2016-files/display/templates/print-board.tpl.php
@@ -3,14 +3,128 @@
 <head lang="en">
   <meta charset="UTF-8">
   <title>Communication Board</title>
-  <link href="static/resources/board.css" rel="stylesheet">
+  <link href="static/resources/print-board.css" rel="stylesheet">
   <!-- If using wih Drupal, say, ensure the page loads this version of jQuery
   rather than the usual one -->
 
 </head>
 <body>
-<div id="board">
-<?php print $content ?>
-</div>
+
+<table class="print-board">
+  <tr>
+    <td class="row-1 col-1"><?php print $cell_1_1 ?></td>
+    <td class="row-1 col-2">&nbsp;</td>
+    <td class="row-1 col-3">&nbsp;</td>
+    <td class="row-1 col-4">&nbsp;</td>
+    <td class="row-1 col-5"><?php print $cell_1_2 ?></td>
+    <td class="row-1 col-6">&nbsp;</td>
+    <td class="row-1 col-7">&nbsp;</td>
+    <td class="row-1 col-8">&nbsp;</td>
+    <td class="row-1 col-9"><?php print $cell_1_3 ?></td>
+  </tr>
+
+
+  <tr>
+    <td class="row-2 col-1">&nbsp;</td>
+    <td class="row-2 col-2">&nbsp;</td>
+    <td class="row-2 col-3">&nbsp;</td>
+    <td class="row-2 col-4">&nbsp;</td>
+    <td class="row-2 col-5">&nbsp;</td>
+    <td class="row-2 col-6">&nbsp;</td>
+    <td class="row-2 col-7">&nbsp;</td>
+    <td class="row-2 col-8">&nbsp;</td>
+    <td class="row-2 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-3 col-1">&nbsp;</td>
+    <td class="row-3 col-2">&nbsp;</td>
+    <td class="row-3 col-3">&nbsp;</td>
+    <td class="row-3 col-4">&nbsp;</td>
+    <td class="row-3 col-5">&nbsp;</td>
+    <td class="row-3 col-6">&nbsp;</td>
+    <td class="row-3 col-7">&nbsp;</td>
+    <td class="row-3 col-8">&nbsp;</td>
+    <td class="row-3 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-4 col-1">&nbsp;</td>
+    <td class="row-4 col-2">&nbsp;</td>
+    <td class="row-4 col-3">&nbsp;</td>
+    <td class="row-4 col-4">&nbsp;</td>
+    <td class="row-4 col-5">&nbsp;</td>
+    <td class="row-4 col-6">&nbsp;</td>
+    <td class="row-4 col-7">&nbsp;</td>
+    <td class="row-4 col-8">&nbsp;</td>
+    <td class="row-4 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-5 col-1"><?php print $cell_2_1 ?></td>
+    <td class="row-5 col-2">&nbsp;</td>
+    <td class="row-5 col-3">&nbsp;</td>
+    <td class="row-5 col-4">&nbsp;</td>
+    <td class="row-5 col-5">&nbsp;</td>
+    <td class="row-5 col-6">&nbsp;</td>
+    <td class="row-5 col-7">&nbsp;</td>
+    <td class="row-5 col-8">&nbsp;</td>
+    <td class="row-5 col-9"><?php print $cell_2_3 ?></td>
+  </tr>
+
+  <tr>
+    <td class="row-6 col-1">&nbsp;</td>
+    <td class="row-6 col-2">&nbsp;</td>
+    <td class="row-6 col-3">&nbsp;</td>
+    <td class="row-6 col-4">&nbsp;</td>
+    <td class="row-6 col-5">&nbsp;</td>
+    <td class="row-6 col-6">&nbsp;</td>
+    <td class="row-6 col-7">&nbsp;</td>
+    <td class="row-6 col-8">&nbsp;</td>
+    <td class="row-6 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-7 col-1">&nbsp;</td>
+    <td class="row-7 col-2">&nbsp;</td>
+    <td class="row-7 col-3">&nbsp;</td>
+    <td class="row-7 col-4">&nbsp;</td>
+    <td class="row-7 col-5">&nbsp;</td>
+    <td class="row-7 col-6">&nbsp;</td>
+    <td class="row-7 col-7">&nbsp;</td>
+    <td class="row-7 col-8">&nbsp;</td>
+    <td class="row-7 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-8 col-1">&nbsp;</td>
+    <td class="row-8 col-2">&nbsp;</td>
+    <td class="row-8 col-3">&nbsp;</td>
+    <td class="row-8 col-4">&nbsp;</td>
+    <td class="row-8 col-5">&nbsp;</td>
+    <td class="row-8 col-6">&nbsp;</td>
+    <td class="row-8 col-7">&nbsp;</td>
+    <td class="row-8 col-8">&nbsp;</td>
+    <td class="row-8 col-9">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td class="row-9 col-1"><?php print $cell_3_1 ?></td>
+    <td class="row-9 col-2">&nbsp;</td>
+    <td class="row-9 col-3">&nbsp;</td>
+    <td class="row-9 col-4">&nbsp;</td>
+    <td class="row-9 col-5"><?php print $cell_3_2 ?></td>
+    <td class="row-9 col-6">&nbsp;</td>
+    <td class="row-9 col-7">&nbsp;</td>
+    <td class="row-9 col-8">&nbsp;</td>
+    <td class="row-9 col-9"><?php print $cell_3_3 ?></td>
+  </tr>
+
+</table>
+
+
+
+
+
 </body>
 </html>
---
+++ test-eg2016-files/display/templates/print-cell.tpl.php
@@ -0,0 +1,8 @@
+<div class="board-cell-content">
+  <?php if (!empty($image)): ?>
+    <img src="<?php print $image ?>"/>
+  <?php endif; ?>
+  <?php if (!empty($display)): ?>
+    <br><?php print $display ?>
+  <?php endif; ?>
+</div>
